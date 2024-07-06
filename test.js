6832c28ae98372dea142e628cb562fefcb8ada91
MojoXQ <quadrizubair7@gmail.com>
2024-07-06 10:59:33 +0100

    Add files via upload
    number 2

--- test.js
+++ test.js
@@ -1,9 +1,39 @@
-// test if a number is even or odd
+// // Create a JavaScript function named calculator.
+// The function should take three parameters: num1, num2, and operator.
+// Use a switch statement to perform the following operations based on the operator:
+// +: Addition
+// -: Subtraction
+// *: Multiplication
+// /: Division
+// %: Modulus
+// Return the result of the operation.
+// If the operator is not one of the above, return "Invalid operator".
 
-let x = 56
 
-if (x % 2 == 0){
-    console.log('This number is even')
-}
-else {console.log('This number is odd')}
-
+function calculator(num1, num2, operator) {
+    return result;
+    
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+      case '%':
+        result = num1 % num2;
+        break;
+      default:
+        return "Invalid operator";
+    }
+  
+    return result;
+  }
+  
+  
